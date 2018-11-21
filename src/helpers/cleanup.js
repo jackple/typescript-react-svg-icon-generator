61171038767195e5605b367b78a55059b898e49a
liupeixin <liupeixin@wisdomgarden.com>
2018-11-21 13:39:22 +0800

    fix clean svg class property

--- src/helpers/cleanup.js
+++ src/helpers/cleanup.js
@@ -10,7 +10,7 @@ module.exports.cleanupSvg = (svg) => {
     .replace(/ fill-rule="#?\w+"/g, '')
     .replace(/ clip-rule="#?\w+"/g, '')
     .replace(/\s{2,}/g, ' ')
-    .replace(/ class="\w+"/g, '')
+    .replace(/ class="\S+"/g, '')
     .replace(/<style>.*<\/style>/g, match => match.replace(/<style>/g, '<style>{ `').replace(/<\/style>/g, '` }</style>'))
     .replace(/viewBox/, 'fill={color} height={height || size} width={width || size} onClick={onClick} style={style} className={className} viewBox');
 };
